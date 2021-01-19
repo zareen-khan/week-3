d1b1d1fa4a927b0759600bdddc7a8cfb6922ba99
zareen-khan <77352563+zareen-khan@users.noreply.github.com>
2021-01-19 13:39:21 -0600

    Initial commit

---
+++ lab/week3.js
@@ -0,0 +1,34 @@
+let numberOfProducts        // expects a Number 
+let products                // expects an Array of product data
+let promotion1Description   // expects a String describing the promotion
+let promotion2Description   // expects a String describing the promotion
+let promotion3Description   // expects a String describing the promotion
+let promotion1Icon          // expects a String (emoji)
+let promotion2Icon          // expects a String (emoji)
+let promotion3Icon          // expects a String (emoji)
+
+// The `database` variable is an existing object that contains our products database.
+// The variables at the top of this code will be used to create the UI for the page.
+// Fill in their values; e.g. set a value to `numberOfProducts` and it will appear on the page.
+
+// promotion1-3 are variables representing randomized promotions at the top of the page.
+// These promotions should change with every page load – assume we don't know the number
+// of promotions in the database. Duplicates are ok.
+// Promotions should be displayed with a corresponding icon.
+// - 🚛 for shipping
+// - 🤑 for discount
+// - 🎰 for contest
+
+// HINTS
+// - Use Array.length to determine the total number of products/promotions in the database
+// - To generate a random number between 0 and x in Javascript:
+//   Math.floor(Math.random() * x)
+
+function pageLoad() {
+  // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
+
+  // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
+  console.log(database)
+
+  //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
+}
